04802650a6e359e500a251220a4650a2b7432f87
maxieo <maxim.ostrovskyw@gmail.com>
2022-08-21 12:37:13 +0300

    main

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+
+const galleryContainer = document.querySelector ('.gallery')
+const cardsGallery = gallaryCreateItem (galleryItems)
+
+galleryContainer.insertAdjacentHTML ('beforeEnd', cardsGallery)
+
+function gallaryCreateItem (galleryItems) {
+    return galleryItems.map (({preview, original, description}) => {
+        return `
+            <a class = "gallery__link" href = "${original}">
+                <img 
+                    class = "gallery__image"
+                    src = "${preview}" 
+                    data-source = "${original}"
+                    alt = "${description}" 
+                />
+            </a>
+        `
+    }).join ('')
+}
+
+ 
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt'
+});

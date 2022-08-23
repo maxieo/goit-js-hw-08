7e597b1157491cd3b4449ee1e5374acf4bd5407f
maxieo <maxim.ostrovskyw@gmail.com>
2022-08-23 16:58:51 +0300

    first try

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,16 @@
 import Player from '@vimeo/player'
+import throttle from 'lodash.throttle';
     const iframe = document.querySelector('iframe');
     const player = new Player(iframe);
 
+    const onPlay = function ({seconds}) {
+        localStorage.setItem ('videoplayer-current-time', seconds)
+    }
+
+    player.on ('timeupdate', throttle(onPlay, 1000))
+
+    const afterTimeUpdate = localStorage.getItem ('videoplayer-current-time')
+
+    if(afterTimeUpdate) {
+        player.setCurrentTime (afterTimeUpdate)
+    }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from "lodash.throttle"
 
+const refs = {
+    form: document.querySelector ('.feedback-form'),
+    textarea: document.querySelector ('.feedback-form textarea')
+}
+
+const STORAGE_KEY = 'feedback-form-state'
+
+savedMessage ()
+const formData = {}
+
+
+refs.form.addEventListener ('submit', onFormSubmit)
+refs.textarea.addEventListener ('input', throttle (onTextarea, 1000))
+
+    refs.form.addEventListener ('input', e => {
+        formData[e.target.name] = e.target.value
+        console.log(formData);
+    })
+
+function onFormSubmit (e) {
+    e.preventDefault ()
+
+    e.currentTarget.reset()
+    localStorage.removeItem (STORAGE_KEY)
+}
+
+function onTextarea (e) {
+    const value = e.target.value
+    localStorage.setItem(STORAGE_KEY, value)
+}
+
+
+function savedMessage () {
+    const save = localStorage.getItem (STORAGE_KEY)
+
+    if(save) {
+        refs.textarea.value = save
+    }
+}
